b181f57eb82079119d59114ba2f5323b627b6862
Dhia' Alhaq Shalabi <dhia.shalabi@gmail.com>
2023-03-22 21:44:09 +0300

    feat: completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import React, { useEffect, useState } from 'react';
+
+function App() {
+    const [dogImage, setDogImage] = useState(null);
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then((response) => response.json())
+            .then((data) => setDogImage(data.message));
+    });
+
+    return (
+        <div>
+            {dogImage ? (
+                <img
+                    src={dogImage}
+                    alt="A Random Dog"
+                />
+            ) : (
+                <p>Loading...</p>
+            )}
+        </div>
+    );
+}
+
+export default App;
